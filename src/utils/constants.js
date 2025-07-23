b92e8360bdbc56a709628513d5249edbfae086f3
garuda-solution <r.seferov@garudasolution.ru>
2025-07-23 14:53:22 -0700

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
-export const API_URL = "https://garuda-solution-api.onrender.com";
+// export const API_URL = "https://garuda-solution-api.onrender.com";
 // export const API_URL = "http://192.168.0.130:5089";
+export const API_URL = "https://garuda-solution-api-1a90.twc1.net";
